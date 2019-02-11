71c3c7995f256a646a140c127c5137f8874bda47
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2019-02-11 12:52:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  # address = "https://vault.example.net:8200"
+}
 resource "vault_auth_backend" "example" {
   type = "github"
   path = "arslan/gitauth"
